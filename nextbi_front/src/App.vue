a373f44bdd02924dd353ed645fead6b5030c40f1
mzza-jung <aragon5198@gmail.com>
2021-12-15 11:08:08 +0900

    toolbar sample

---
+++ nextbi_front/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer />
+    </v-app-bar>
+    
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-main>
+        <HelloWorld />
+      </v-main>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    drawer: false
+    //
+  }),
+};
+</script>
